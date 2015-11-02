b4c083039c2cbf3edb99f8190fa3ced2d7ff8560
mhuisi <mhuisi@protonmail.com>
2015-11-03 00:46:34 +0100

    Added some utility functions for collision detection

--- src/game/game.go
+++ src/game/game.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	"image"
+	"math"
 	"time"
 )
 
@@ -12,12 +13,56 @@ type Field struct {
 	Size  image.Point
 }
 
+type Circle struct {
+	Location image.Point
+	Radius   float64
+}
+
+func InCircle(p image.Point, c Circle) bool {
+	vecPC := p.Sub(p, c.Location)
+	// |vecPC|
+	dist := math.Sqrt(math.Pow(vecPC.X, 2) + math.Pow(vecPC.Y, 2))
+	return dist < c.Radius
+}
+
+func Colliding(c1, c2 Circle) bool {
+	if c1.Radius > c2.Radius {
+		return InCircle(c2.Location, c1)
+	}
+	return InCircle(c1.Location, c2)
+}
+
+type CircleTrajectory struct {
+	Start  image.Point
+	End    image.Point
+	Radius float64
+}
+
 type Cell struct {
-	Capacity            int
+	Capacity            chan int
 	ReplicationInterval time.Duration
 	Stationed           Vires
 	Owner               *Player
-	Location            image.Point
+	Shape               Circle
+}
+
+func (c *Cell) Replicate() {
+	cp := <-c.Capacity
+	c.Capacity <- cp + 1
+}
+
+func (c *Cell) ReplicateContinuously(quit chan struct{}) {
+	t := time.NewTicker(c.ReplicationInterval)
+	go func() {
+		select {
+		case <-quit:
+			t.Stop()
+			return
+		default:
+			c.Replicate()
+			<-t.C
+		}
+	}()
 }
 
 type Player struct {
@@ -26,7 +71,9 @@ type Player struct {
 }
 
 type Movement struct {
-	Moving   Vires
-	Target   Cell
-	Location image.Point
+	Moving Vires
+	Target *Cell
+	Shape  Circle
+	// [Speed] = dots/s
+	Speed float64
 }
