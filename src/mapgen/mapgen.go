cb19ab041a7210d1fb53eda8d2a1d6a464fb97f4
amazinq <steffen.wissmann@hotmail.com>
2016-02-12 19:40:48 +0100

    Mapgen finished still needs to be tested

--- src/mapgen/mapgen.go
+++ src/mapgen/mapgen.go
@@ -11,12 +11,13 @@ import (
 )
 
 const (
-	CellMinimumSize  = 4
-	CellMaximumSize  = 24
-	MaximumXPosition = 800
-	MaximumYPosition = 800
-	NumberOfCells    = 50
-	DistanceFactor   = 1
+	CellMinimumSize           = 4
+	CellMaximumSize           = 24
+	MaximumXPosition          = 800
+	MaximumYPosition          = 800
+	NumberOfCells             = 50
+	DistanceFactor            = 1
+	NumberOfMapsPerGeneration = 8
 )
 
 type Cell struct {
@@ -31,31 +32,71 @@ type Map struct {
 }
 
 var maps []Map
-var currentLowestFitness Map
-var currentSecondBestFitness Map
+var currentLowestFitness *Map
+var currentSecondBestFitness *Map
 
 func setFitnesses() {
 	for _, currMap := range maps {
 
-		if currentLowestFitness.cells == nil {
-			currentLowestFitness = currMap
-		} else if currentSecondBestFitness.cells == nil && currMap.fitness > currentLowestFitness.fitness {
-			currentSecondBestFitness = currMap
-		} else if currentSecondBestFitness.cells == nil && currMap.fitness <= currentLowestFitness.fitness {
+		if currentLowestFitness == nil {
+			currentLowestFitness = &currMap
+		} else if currentSecondBestFitness == nil && currMap.fitness > currentLowestFitness.fitness {
+			currentSecondBestFitness = &currMap
+		} else if currentSecondBestFitness == nil && currMap.fitness <= currentLowestFitness.fitness {
 			currentSecondBestFitness = currentLowestFitness
-			currentLowestFitness = currMap
+			currentLowestFitness = &currMap
 		} else if currMap.fitness < currentLowestFitness.fitness {
 			currentSecondBestFitness = currentLowestFitness
-			currentLowestFitness = currMap
+			currentLowestFitness = &currMap
 		} else if currMap.fitness > currentLowestFitness.fitness && currMap.fitness < currentSecondBestFitness.fitness {
-			currentSecondBestFitness = currMap
+			currentSecondBestFitness = &currMap
 		}
 	}
 }
 
 func generateMap() {
-	//generationSuccessful := false
-
+	generationSuccessful := false
+	for generationSuccessful == false {
+		maps = nil
+		currentLowestFitness.cells = nil
+		currentLowestFitness.fitness = 0
+		currentSecondBestFitness.cells = nil
+		currentSecondBestFitness.fitness = 0
+		for i := 0; i < NumberOfMapsPerGeneration; i++ {
+			currentCellList := generateCellList()
+			maps = append(maps, Map{currentCellList, calculateFitnesses(currentCellList)})
+		}
+		setFitnesses()
+		numberOfGenerations := 0
+		for currentLowestFitness.fitness != 0 && numberOfGenerations < 10000 {
+			crossDivider := rand.Int31n(NumberOfCells)
+			var childCellList []Cell
+			for i := int32(0); i < crossDivider; i++ {
+				if rand.Int31n(100) >= 2 {
+					childCellList = append(childCellList, currentLowestFitness.cells[i])
+				} else {
+					childCellList = append(childCellList, Cell{rand.Int31n(MaximumXPosition), rand.Int31n(MaximumYPosition), rand.Int31n(CellMaximumSize-CellMinimumSize) + CellMinimumSize})
+				}
+			}
+			for i := crossDivider; i < NumberOfCells; i++ {
+				if rand.Int31n(100) >= 2 {
+					childCellList = append(childCellList, currentSecondBestFitness.cells[i])
+				} else {
+					childCellList = append(childCellList, Cell{rand.Int31n(MaximumXPosition), rand.Int31n(MaximumYPosition), rand.Int31n(CellMaximumSize-CellMinimumSize) + CellMinimumSize})
+				}
+			}
+			for i, currentMap := range maps {
+				if &currentMap == currentSecondBestFitness {
+					maps = append(maps[:i-1], maps[i+1:]...)
+					maps = append(maps, Map{childCellList, calculateFitnesses(childCellList)})
+				}
+			}
+			numberOfGenerations++
+			if numberOfGenerations < 10000 {
+				generationSuccessful = true
+			}
+		}
+	}
 	// TODO: return generated map here
 }
 
