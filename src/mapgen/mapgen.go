0ed7b5882d6afcf5e6b39492f8516f2d60ef13f7
amazinq <steffen.wissmann@hotmail.com>
2016-02-23 09:49:41 +0100

    Rewrote some code

--- src/mapgen/mapgen.go
+++ src/mapgen/mapgen.go
@@ -6,6 +6,8 @@ package mapgen
 //channels = append(channels, Channel{name:"some other name"})
 
 import (
+	"github.com/mhuisi/vires/src/ent"
+	"github.com/mhuisi/vires/src/vec"
 	"math"
 	"math/rand"
 )
@@ -31,64 +33,72 @@ type Map struct {
 	fitness float64
 }
 
-var maps []Map
-var currentLowestFitness *Map
-var currentSecondBestFitness *Map
+type Generation struct {
+	maps                     []Map
+	currentLowestFitness     *Map
+	currentSecondBestFitness *Map
+}
 
-func setFitnesses() {
-	for _, currMap := range maps {
+func newGeneration(maps []Map, lowestFitness *Map, secondLowestFitness *Map) *Generation {
+	return &Generation{
+		maps:                     maps,
+		currentLowestFitness:     lowestFitness,
+		currentSecondBestFitness: secondLowestFitness,
+	}
+}
 
-		if currentLowestFitness == nil {
-			currentLowestFitness = &currMap
-		} else if currentSecondBestFitness == nil && currMap.fitness > currentLowestFitness.fitness {
-			currentSecondBestFitness = &currMap
-		} else if currentSecondBestFitness == nil && currMap.fitness <= currentLowestFitness.fitness {
-			currentSecondBestFitness = currentLowestFitness
-			currentLowestFitness = &currMap
-		} else if currMap.fitness < currentLowestFitness.fitness {
-			currentSecondBestFitness = currentLowestFitness
-			currentLowestFitness = &currMap
-		} else if currMap.fitness > currentLowestFitness.fitness && currMap.fitness < currentSecondBestFitness.fitness {
-			currentSecondBestFitness = &currMap
+func setFitnesses(generation *Generation) {
+	for _, currMap := range generation.maps {
+
+		if generation.currentLowestFitness == nil {
+			generation.currentLowestFitness = &currMap
+		} else if generation.currentSecondBestFitness == nil && currMap.fitness > generation.currentLowestFitness.fitness {
+			generation.currentSecondBestFitness = &currMap
+		} else if generation.currentSecondBestFitness == nil && currMap.fitness <= generation.currentLowestFitness.fitness {
+			generation.currentSecondBestFitness = generation.currentLowestFitness
+			generation.currentLowestFitness = &currMap
+		} else if currMap.fitness < generation.currentLowestFitness.fitness {
+			generation.currentSecondBestFitness = generation.currentLowestFitness
+			generation.currentLowestFitness = &currMap
+		} else if currMap.fitness > generation.currentLowestFitness.fitness && currMap.fitness < generation.currentSecondBestFitness.fitness {
+			generation.currentSecondBestFitness = &currMap
 		}
 	}
 }
 
-func generateMap() *Map {
+func generateMap(numberOfPlayers int) ([]ent.Circle, []int) {
 	generationSuccessful := false
+	generation := newGeneration(nil, nil, nil)
 	for generationSuccessful == false {
-		maps = nil
-		currentLowestFitness.cells = nil
-		currentLowestFitness.fitness = 0
-		currentSecondBestFitness.cells = nil
-		currentSecondBestFitness.fitness = 0
+		generation := newGeneration(nil, nil, nil)
 		for i := 0; i < NumberOfMapsPerGeneration; i++ {
 			currentCellList := generateCellList()
-			maps = append(maps, Map{currentCellList, calculateFitnesses(currentCellList)})
+			generation.maps = append(generation.maps, Map{currentCellList, calculateFitnesses(currentCellList)})
+
 		}
-		setFitnesses()
+		setFitnesses(generation)
 		numberOfGenerations := 0
-		for currentLowestFitness.fitness != 0 && numberOfGenerations < 10000 {
+		for generation.currentLowestFitness.fitness != 0 && numberOfGenerations < 10000 {
 			crossDivider := rand.Int31n(NumberOfCells)
 			var childCellList []Cell
 			for i := int32(0); i < crossDivider; i++ {
 				if rand.Int31n(100) >= 2 {
-					childCellList = append(childCellList, currentLowestFitness.cells[i])
+					childCellList = append(childCellList, generation.currentLowestFitness.cells[i])
 				} else {
 					childCellList = append(childCellList, Cell{rand.Int31n(MaximumXPosition), rand.Int31n(MaximumYPosition), rand.Int31n(CellMaximumSize-CellMinimumSize) + CellMinimumSize})
 				}
 			}
 			for i := crossDivider; i < NumberOfCells; i++ {
 				if rand.Int31n(100) >= 2 {
-					childCellList = append(childCellList, currentSecondBestFitness.cells[i])
+					childCellList = append(childCellList, generation.currentSecondBestFitness.cells[i])
 				} else {
 					childCellList = append(childCellList, Cell{rand.Int31n(MaximumXPosition), rand.Int31n(MaximumYPosition), rand.Int31n(CellMaximumSize-CellMinimumSize) + CellMinimumSize})
 				}
 			}
-			for i, currentMap := range maps {
-				if &currentMap == currentSecondBestFitness {
-					maps = append(maps[:i-1], maps[i+1:]...)
-					maps = append(maps, Map{childCellList, calculateFitnesses(childCellList)})
+			for i, currentMap := range generation.maps {
+				if &currentMap == generation.currentSecondBestFitness {
+					generation.maps = append(generation.maps[:i-1], generation.maps[i+1:]...)
+					generation.maps = append(generation.maps, Map{childCellList, calculateFitnesses(childCellList)})
 				}
 			}
 			numberOfGenerations++
@@ -97,7 +107,11 @@ func generateMap() *Map {
 			}
 		}
 	}
-	return currentLowestFitness
+	var circles []ent.Circle
+	for _, currentCell := range generation.currentLowestFitness.cells {
+		circles = append(circles, ent.Circle{vec.V{float64(currentCell.xPosition), float64(currentCell.yPosition)}, float64(currentCell.size)})
+	}
+	return circles, nil
 }
 
 func calculateFitnesses(cells []Cell) float64 {
